ea80bd7d82242d7b9dc376919b8c6a59c9c8434b
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-04-21 15:58:24 +0200

    init

---
+++ src/test/java/dev/imaretarded/gino/GinoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.imaretarded.gino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
