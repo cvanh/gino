297749437a9a61fdf8bd9c270a3d67a26322e48e
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-04-21 16:18:23 +0200

    added example test

--- src/main/java/dev/imaretarded/gino/GinoApplication.java
+++ src/main/java/dev/imaretarded/gino/GinoApplication.java
@@ -11,7 +11,8 @@ public class GinoApplication {
     public static void main(String[] args) {
       SpringApplication.run(GinoApplication.class, args);
     }
-    @GetMapping("/hello")
+
+    @GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
--- src/test/java/dev/imaretarded/gino/GinoApplicationTests.java
+++ src/test/java/dev/imaretarded/gino/GinoApplicationTests.java
@@ -1,13 +1,29 @@
 package dev.imaretarded.gino;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+// @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class GinoApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	// @Test
+	// void contextLoads() {
+	// }
+
 	@Test
-	void contextLoads() {
+	void test_hello() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk());
 	}
 
 }
